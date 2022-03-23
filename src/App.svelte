c2e6a4a10623c9333e212724d2acb27733073bce
Kasper <kasperkh.kh@gmail.com>
2022-03-23 01:26:46 +0100

    Tauri template

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<h1>Tetragrade</h1>
+
+<style lang="sass">
+  :global(body)
+    margin: 100px 0px
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 18px
+    background-color: #111318
+    color: #f2f2f2
+    text-align: center
+</style>
